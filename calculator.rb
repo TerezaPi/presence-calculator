abb257127d84c6904dbbdf3f4b31cfd8b6448efb
Tereza Pidrmanova <terezapidrmanova@gmail.com>
2017-12-10 17:36:31 -0800

    parsing data into array of hashes

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,41 @@
 file_name = "data"
-puts File.read(file_name)
+#puts File.read(file_name)
+
+file_content = File.read(file_name)
+
+#puts file_content
+
+# parsing text lines into hashes
+# date
+# type
+# location
+
+# preparing array of items
+items = []
+file_content.split("\n").each do |line|
+  splitted_line = line.split(" ")
+  item = {
+     "date" => splitted_line[0],
+     "type" => splitted_line[1],
+     "location" => splitted_line[2]
+  }
+  items.push item
+end
+puts items
+
+
+#hash = key => value
+
+# item = {
+#   "date" => "1988-04-20",
+#   "type" => "arrival",
+#   "location" => "NYC"
+# }
+#
+# #puts item.keys[1]
+#
+# item.keys.each do |key|
+#   puts key
+#   puts "ahoy"
+#
+# end
